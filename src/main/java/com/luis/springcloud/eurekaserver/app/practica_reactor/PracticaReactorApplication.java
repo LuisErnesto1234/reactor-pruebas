55f8cb5d291d6359a15c5eb87b86f35fa7a129dc
L <145403083+LuisErnesto1234@users.noreply.github.com>
2025-08-17 17:43:06 -0500

    Ejercicios primarios - simples

---
+++ src/main/java/com/luis/springcloud/eurekaserver/app/practica_reactor/PracticaReactorApplication.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.eurekaserver.app.practica_reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticaReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticaReactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/luis/springcloud/eurekaserver/app/practica_reactor/PracticaReactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis.springcloud.eurekaserver.app.practica_reactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticaReactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
